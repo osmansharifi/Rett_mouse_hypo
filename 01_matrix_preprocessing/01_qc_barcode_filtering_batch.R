e24ada3d261f06d7dd88e923219bfe0d26a551f0
Shenyu Wang <122090888@link.cuhk.edu.cn>
2026-07-21 11:23:00 -0700

    update barcode qc metric visualization and documentation
    update QC_Count_Feature_raw, create doc for global threshold, delete post-filtering for percent.mt visualization for its lack of information

--- 01_matrix_preprocessing/01_qc_barcode_filtering_batch.R
+++ 01_matrix_preprocessing/01_qc_barcode_filtering_batch.R
@@ -11,35 +11,35 @@ library(dplyr)
 library(tibble)
 
 # Set path and load data
-setwd("/quobyte/lasallegrp/Osman/shenyu/02_seurat_objects")
-seu <- readRDS("seu_metadata.rds")
+DATA_DIR <- "/quobyte/lasallegrp/Osman/shenyu/02_seurat_objects"
+FIG_DIR  <- "/quobyte/lasallegrp/Osman/shenyu/03_figures"
+seu <- readRDS(file.path(DATA_DIR, "seu_metadata.rds"))
 
-## metadata: study design
+# metadata: study design
 head(seu)
 unique(seu$orig.ident) 
 table(unique(seu@meta.data[, c("orig.ident", "sex", "time_point")])[, c("sex", "time_point")])
 
 
-setwd("/quobyte/lasallegrp/Osman/shenyu/03_figures")
 ### 1. Pre-filtering Exploratory Analysis --------------------------------------
 summary(seu$nFeature_RNA)
 summary(seu$nCount_RNA)
 summary(seu$percent.mt)
 
-# Sample Sequencing Depth
-depth_df <- seu@meta.data %>%
+# Sample Total UMI Count Distribution
+df <- seu@meta.data %>%
   group_by(orig.ident) %>%
-  summarise(Total_Depth = sum(nCount_RNA))
-p_depth <- ggplot(depth_df, aes(x = reorder(orig.ident, -Total_Depth), y = Total_Depth)) +
+  summarise(Total_UMI = sum(nCount_RNA))
+p <- ggplot(df, aes(x = reorder(orig.ident, -Total_UMI), y = Total_UMI)) +
   geom_bar(stat = "identity", fill = "steelblue") +
   theme_minimal() +
-  labs(title = "Total Sequencing Depth per Sample",
+  labs(title = "Total UMI Count per Sample",
        x = "Sample",
        y = "Total UMI Count") +
   theme(axis.text.x = element_text(angle = 45, hjust = 1, vjust = 1))
-ggplot2::ggsave("Total_Sequencing_Depth.pdf", plot = p_depth, height = 8.5, width = 10)
+ggplot2::ggsave(file.path(FIG_DIR, "Sample_UMI_Count.pdf"), plot = p, height = 8.5, width = 10)
 
-# Plot distributions (scCustomize)
+# Plot QC Covariate Distributions (scCustomize)
 Idents(seu) <- "scRTT_hypo"
 qc_features <- c("nCount_RNA", "nFeature_RNA", "percent.mt")
 lapply(qc_features, function(feature) {
@@ -49,20 +49,20 @@ lapply(qc_features, function(feature) {
   message(glue("Successfully saved: {file_name}"))
 })
 
-# Violin plot
+# Violin plot (Seurat)
 p <- VlnPlot(seu, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3, raster=FALSE)
-ggplot2::ggsave("violinplot_RNA.pdf", plot = p, height = 8.5, width = 10)
+ggplot2::ggsave(file.path(FIG_DIR, "violinplot_RNA.pdf"), plot = p, height = 8.5, width = 10)
 
-# Pre-filter Dependencies (Scatter)
-plot1 <- FeatureScatter(seu, feature1 = "nCount_RNA", feature2 = "percent.mt", group.by = NULL, raster=FALSE) # r = .07
+# Pre-filter Dependencies (Seurat)
+plot1 <- FeatureScatter(seu, feature1 = "nCount_RNA", feature2 = "percent.mt", group.by = NULL, raster=FALSE) # r = -.07
 plot2 <- FeatureScatter(seu, feature1 = "nCount_RNA", feature2 = "nFeature_RNA", group.by = NULL, raster=FALSE) # r = .98
 p_scatter <- plot1 + plot2
-ggplot2::ggsave("featurescatter_RNA.pdf", plot = p_scatter, height = 8.5, width = 10)
+ggplot2::ggsave(file.path(FIG_DIR, "featurescatter_RNA.pdf"), plot = p_scatter, height = 8.5, width = 10)
 
 
 ### 2. Barcode Filtering (Adaptive & Hard Thresholds) --------------------------
 
-# Explore MAD-based thresholds
+# Explore MAD-based thresholds (scater)
 qc.nCount_RNA <- isOutlier(seu$nCount_RNA, log = TRUE, type = "both", batch = seu$orig.ident, nmads = 5)
 umi_lower_limit <- attr(qc.nCount_RNA, "thresholds")["lower", ]
 umi_upper_limit <- attr(qc.nCount_RNA, "thresholds")["higher", ]
@@ -75,6 +75,25 @@ gene_upper_limit <- attr(qc.nFeature_RNA, "thresholds")["higher", ]
 seu$discard <- qc.nCount_RNA | qc.nFeature_RNA | (seu$percent.mt > 1)
 seu_filtered <- subset(seu, subset = !discard)
 head(seu)
+
+# Global threshold among 28 samples
+global_gene_low  <- min(gene_lower_limit, na.rm = TRUE) # 48.99
+global_gene_high <- max(gene_upper_limit, na.rm = TRUE) # 75170.59
+global_umi_low   <- min(umi_lower_limit, na.rm = TRUE) # 27.54
+global_umi_high  <- max(umi_upper_limit, na.rm = TRUE) # 298097.92
+
+global_thresh_df <- data.frame(
+  Metric = c("global_gene_low", "global_gene_high", "global_umi_low", "global_umi_high"),
+  Global_Value = c(
+    min(gene_lower_limit, na.rm = TRUE),
+    max(gene_upper_limit, na.rm = TRUE),
+    min(umi_lower_limit, na.rm = TRUE),
+    max(umi_upper_limit, na.rm = TRUE)
+  ),
+  stringsAsFactors = FALSE
+)
+print(global_thresh_df)
+write.csv(global_thresh_df, file = file.path(FIG_DIR, "barcode_qc_globalthreshold.csv"), row.names = FALSE)
 
 # Document cell filtration statistics
 thresh_df <- data.frame(
@@ -93,63 +112,64 @@ qc_summary <- seu@meta.data %>%
     Discard_Gene_Count = sum(nFeature_RNA < gene_lower_limit[orig.ident] | nFeature_RNA > gene_upper_limit[orig.ident], na.rm = TRUE),
     Total_Discard_Cells = sum(discard, na.rm = TRUE)
   )
-final_summary <- depth_df %>%
+final_summary <- df %>%
   left_join(thresh_df, by = "orig.ident") %>%
   left_join(qc_summary, by = "orig.ident") %>%
-  arrange(desc(Total_Depth))
+  arrange(desc(Total_UMI))
+write.csv(final_summary, file.path(FIG_DIR, "barcode_qc_summary.csv"), row.names = FALSE)
 print(head(final_summary))
 sum(final_summary$Total_Discard_Cells) # 193
-write.csv(final_summary, "SampleSequencingDepth_QCSummary.csv", row.names = FALSE)
 
 
 ### 3. Post-filtering QC Re-assessment -------------------------
 
-# Violin plot
+# Violin plot (Seurat)
 p <- VlnPlot(seu, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3, group.by = "discard", raster = FALSE)
-ggplot2::ggsave("violinplot_RNA_filtered.pdf", plot = p, height = 8.5, width = 10)
+ggplot2::ggsave(file.path(FIG_DIR, "violinplot_RNA_filtered.pdf"), plot = p, height = 8.5, width = 10)
 
-# Global threshold among 28 samples
-global_gene_low  <- min(gene_lower_limit, na.rm = TRUE) # 48.99
-global_gene_high <- max(gene_upper_limit, na.rm = TRUE) # 75170.59
-global_umi_low   <- min(umi_lower_limit, na.rm = TRUE) # 27.54
-global_umi_high  <- max(umi_upper_limit, na.rm = TRUE) # 298097.92
-
-# UMI vs Gene with threshold lines
+# UMI vs Gene (scCustomize)
 p1 <- QC_Plot_UMIvsGene(seu, 
                         x_axis_label = "UMIs per Nucleus", y_axis_label = "Genes per Nucleus",
                         group.by = "discard",
                         raster = FALSE) +
   ggplot2::labs(title = "Genes vs. UMIs per Nucleus")
-ggplot2::ggsave("QC_Plot_UMIvsGene_filtered.pdf", plot = p1, height = 8.5, width = 10)
+ggplot2::ggsave(file.path(FIG_DIR, "QC_Plot_UMIvsGene_raw.pdf"), plot = p1, height = 8.5, width = 10)
 
-# percent.mt vs Gene with threshold lines
+p1 <- QC_Plot_UMIvsGene(seu_filtered, 
+                        x_axis_label = "UMIs per Nucleus", y_axis_label = "Genes per Nucleus",
+                        group.by = "discard",
+                        raster = FALSE) +
+  ggplot2::labs(title = "Genes vs. UMIs per Nucleus")
+ggplot2::ggsave(file.path(FIG_DIR, "QC_Plot_UMIvsGene_filtered.pdf"), plot = p1, height = 8.5, width = 10)
+
+# percent.mt vs Gene (scCustomize)
+# r = -.09
 p2 <- QC_Plot_GenevsFeature(seu, 
                             feature1 = "percent.mt", 
                             high_cutoff_feature = 1, 
                             group.by = "discard",
                             y_axis_label = "Genes per Nucleus", x_axis_label = "percent.mt per Nucleus",
-                            raster = FALSE) # # r = -.09
-ggplot2::ggsave("QC_Plot_percent.mtvsGene_raw.pdf", plot = p2, height = 8.5, width = 10)
+                            raster = FALSE) 
+ggplot2::ggsave(file.path(FIG_DIR, "QC_Plot_percent.mtvsGene_raw.pdf"), plot = p2, height = 8.5, width = 10)
+
+# r = -.12
 p2 <- QC_Plot_GenevsFeature(seu_filtered, 
                             feature1 = "percent.mt", 
                             high_cutoff_feature = 1, 
+                            group.by = "discard",
                             y_axis_label = "Genes per Nucleus", x_axis_label = "percent.mt per Nucleus",
-                            raster = FALSE) # r = -.12
-ggplot2::ggsave("QC_Plot_percent.mtvsGene_filtered.pdf", plot = p2, height = 8.5, width = 10) 
-
-# Mito histogram post-filtering
-p3 <- QC_Histogram(seu, features = "percent.mt", low_cutoff = 1)
-ggplot2::ggsave("QC_Histogram_percent.mt_filtered.pdf", plot = p3, height = 8.5, width = 10)
+                            raster = FALSE) 
+ggplot2::ggsave(file.path(FIG_DIR, "QC_Plot_percent.mtvsGene_filtered.pdf"), plot = p2, height = 8.5, width = 10) 
 
 
 ### 4. Sex difference in Median number of Genes and UMIs per Nucleus -------------------------------------------
 p_sex_gene <- Plot_Median_Genes(seu_filtered, group.by = "sex")
-ggplot2::ggsave("Plot_Median_Genes_sex_filtered.pdf", plot = p_sex_gene, height = 8.5, width = 10)
+ggplot2::ggsave(file.path(FIG_DIR, "Plot_Median_Genes_sex_filtered.pdf"), plot = p_sex_gene, height = 8.5, width = 10)
 
 p_sex_umi <- Plot_Median_UMIs(seu_filtered, group.by = "sex")
-ggplot2::ggsave("Plot_Median_UMIs_sex_filtered.pdf", plot = p_sex_umi, height = 8.5, width = 10)
+ggplot2::ggsave(file.path(FIG_DIR, "Plot_Median_UMIs_sex_filtered.pdf"), plot = p_sex_umi, height = 8.5, width = 10)
 
 
 ### 5. Save Cleaned Data -------------------------------------------------------
-setwd("/quobyte/lasallegrp/Osman/shenyu/02_seurat_objects")
-saveRDS(seu_filtered, file = "seu_filtered_batch.rds")
+Idents(seu_filtered)
+saveRDS(seu_filtered, file = file.path(DATA_DIR,"seu_filtered_batch.rds"))
